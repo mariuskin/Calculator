b10dc5c26b21ffca489cbcad276c74790e7b9dc3
Marius <marius.kinderis@hotmail.fr>
2016-07-16 19:53:33 +0300

    finished the layout

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+enum modes {
+    case NOT_SET
+    case ADDITION
+    case SUBSTRACTION
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString:String = "0"
+    var currentMode:modes = modes.NOT_SET
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+    
+    func tappedNumber(num:Int) {
+        <#function body#>
+    }
+    
+    func updateText() {
+        <#function body#>
+    }
+    
+    func changeMode(newMode:modes) {
+        <#function body#>
+    }
+    
+    @IBAction func tappedPlus(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
