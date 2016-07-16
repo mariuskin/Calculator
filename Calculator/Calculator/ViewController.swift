4e75e7b4341ecb6eec81d3a91de185d1de600f88
Marius <marius.kinderis@hotmail.fr>
2016-07-16 20:10:04 +0300

    can add numbers on the screen

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -25,19 +25,49 @@ class ViewController: UIViewController {
     var lastButtonWasMode:Bool = false
     
     func tappedNumber(num:Int) {
-        <#function body#>
+        labelString = labelString.stringByAppendingString("\(num)")
+        updateText()
     }
     
     func updateText() {
-        <#function body#>
+        label.text = labelString
     }
     
     func changeMode(newMode:modes) {
-        <#function body#>
+//        <#function body#>
     }
     
+    
+    @IBAction func tapped0(sender: AnyObject) {tappedNumber(0)}
+    @IBAction func tapped1(sender: AnyObject) {tappedNumber(1)}
+    @IBAction func tapped2(sender: AnyObject) {tappedNumber(2)}
+    @IBAction func tapped3(sender: AnyObject) {tappedNumber(3)}
+    @IBAction func tapped4(sender: AnyObject) {tappedNumber(4)}
+    @IBAction func tapped5(sender: AnyObject) {tappedNumber(5)}
+    @IBAction func tapped6(sender: AnyObject) {tappedNumber(6)}
+    @IBAction func tapped7(sender: AnyObject) {tappedNumber(7)}
+    @IBAction func tapped8(sender: AnyObject) {tappedNumber(8)}
+    @IBAction func tapped9(sender: AnyObject) {tappedNumber(9)}
+    
+    
+    
+    
+
     @IBAction func tappedPlus(sender: AnyObject) {
     }
+    
+    @IBAction func tappedMinus(sender: AnyObject) {
+    }
+    
+    @IBAction func tappedEquals(sender: AnyObject) {
+    }
+    
+    @IBAction func tappedClear(sender: AnyObject) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
